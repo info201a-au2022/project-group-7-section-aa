bf3f266945aa556132905deb9d00b56703a4066a
Shivesh <shiveshummat@gmail.com>
2022-11-14 13:57:26 -0800

    FINAL DATA

--- source/aggregate_table.R
+++ source/aggregate_table.R
@@ -1,2 +1,21 @@
 ### Use group_by() to create table of aggregate info
 data <- read.csv("taxdata.csv")
+total_income <- sum(select(data, Adjusted.Gross.Income))
+total_taxes <- sum(select(data, Total.taxes.paid.amount))
+summary <- data %>% 
+  group_by(Adjusted.Size.Gross.Income.Category) %>% 
+  summarize("Total Adjusted Gross Income" = 
+              sum(Adjusted.Gross.Income),
+            "Total Number of Returns Filed" = 
+              sum(Number.of.Single.Returns) + 
+              sum(Number.of.Head.of.Household.Returns) +
+              sum(Number.of.Joint.Returns),
+            "Total Taxes Paid" =
+              sum(Total.taxes.paid.amount),
+            "Percent of Total Adjusted Gross Income Paid in Taxes" =
+              (sum(Total.taxes.paid.amount))/(sum(Adjusted.Gross.Income)),
+            "Tax Bracket Percent of Overall Total Income" =
+              sum(Adjusted.Gross.Income)/total_income)
+
+              
+              
