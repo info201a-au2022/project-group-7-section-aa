96fd3f656c4d52aeca5516a827a3ba790edf1cff
Ismail Ibrahim <ismailahmedibrahim@Ismails-MacBook-Pro.local>
2022-11-14 20:40:12 -0800

    summary info

--- source/summary_info.R
+++ source/summary_info.R
@@ -1,1 +1,16 @@
 ### Find summary info for dynamic paragraph
+library(dplyr)
+filename <- "tax_returns"
+df <- read.csv("taxdata.csv")
+# tax_returns.R 
+# A source file that takes in a dataset and returns a list of info about it:
+tax_returns <- list()
+tax_returns$num_observations <- nrow(df)
+tax_returns$num_features <- ncol(df)
+tax_returns$num_zipcodes <- unique(df$Zip.Code) %>% 
+  length()
+tax_returns$numstates <- unique(df$State) %>% 
+  length()
+tax_returns$numoftotalreturns <- sum(df$Number.of.Single.Returns) + 
+  sum(df$Number.of.Head.of.Household.Returns) + 
+  sum(df$Number.of.Joint.Returns)
