822e4c7950bd17baf7fb036163857bdd663e2065
Shivesh <shiveshummat@gmail.com>
2022-11-14 20:41:02 -0800

    aggregate table, chart 3

--- source/aggregate_table.R
+++ source/aggregate_table.R
@@ -1,21 +1,31 @@
-### Use group_by() to create table of aggregate info
 data <- read.csv("taxdata.csv")
+# Adjusted Gross Income is the number IRS uses to calculate income tax
 total_income <- sum(select(data, Adjusted.Gross.Income))
-total_taxes <- sum(select(data, Total.taxes.paid.amount))
-summary <- data %>% 
-  group_by(Adjusted.Size.Gross.Income.Category) %>% 
-  summarize("Total Adjusted Gross Income" = 
-              sum(Adjusted.Gross.Income),
-            "Total Number of Returns Filed" = 
-              sum(Number.of.Single.Returns) + 
-              sum(Number.of.Head.of.Household.Returns) +
-              sum(Number.of.Joint.Returns),
-            "Total Taxes Paid" =
-              sum(Total.taxes.paid.amount),
-            "Percent of Total Adjusted Gross Income Paid in Taxes" =
-              (sum(Total.taxes.paid.amount))/(sum(Adjusted.Gross.Income)),
-            "Tax Bracket Percent of Overall Total Income" =
-              sum(Adjusted.Gross.Income)/total_income)
+total_taxes <- sum(select(data, Total.tax.payments.amount))
+data <- data %>% mutate(Adjusted.Size.Gross.Income.Category = 
+         case_when(Adjusted.Size.Gross.Income.Category == "under $25,000" ~ "1. Under $25,000",
+                   Adjusted.Size.Gross.Income.Category == "$25,000 to $50,000" ~ "2. $25,000 to $50,000",
+                   Adjusted.Size.Gross.Income.Category == "$50,000 to $75,000" ~ "3. $50,000 to $75,000",
+                   Adjusted.Size.Gross.Income.Category == "$75,000 to $100,000" ~ "4. $75,000 to $100,000",
+                   Adjusted.Size.Gross.Income.Category == "$100,000 to $200,000" ~ "5. $100,000 to $200,000",
+                   Adjusted.Size.Gross.Income.Category == "$200,000 or more" ~ "6. $200,000 or more"))
 
-              
-              
+aggregate_table <- data %>% 
+  rename("Tax.Bracket.Status" = "Adjusted.Size.Gross.Income.Category") %>% 
+  group_by(Tax.Bracket.Status) %>% 
+  summarize("Total Number of Returns Filed (in millions)" = 
+              round((sum(Number.of.Single.Returns) + 
+                       sum(Number.of.Head.of.Household.Returns) +
+                       sum(Number.of.Joint.Returns))/1000000, 2),
+            "Total Adjusted Gross Income (in billions of dollars)" = 
+              round(sum(Adjusted.Gross.Income)/1000000, 2),
+            "Percentage of US Total Income" =
+              100 * round(sum(Adjusted.Gross.Income)/total_income, 4),
+            "Total Tax Payments (in billions of dollars)" =
+              round(sum(Total.tax.payments.amount)/1000000, 2),
+            "Percentage of US Total Taxes" =
+              100 * round(sum(Total.tax.payments.amount)/total_taxes, 4),
+            "Effective Tax Rate Percent"=
+              100 * round(sum(Total.tax.payments.amount)/sum(Adjusted.Gross.Income), 4))
+
+
