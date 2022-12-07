e5324afe615a72cad8fdec33a565b097e881c679
Shivesh <shiveshummat@gmail.com>
2022-12-07 11:43:51 -0800

    shiny updates

--- shinyapp/app_ui.R
+++ shinyapp/app_ui.R
@@ -6,20 +6,10 @@ library(shinythemes)
 ### HOME PAGE
 home_panel <- tabPanel(
   title = "Home",
-  titlePanel("Home Title"),
+  titlePanel("An Analysis of Taxes in the US"),
   fluidPage(
     img("", src = "https://s.yimg.com/ny/api/res/1.2/1AVjgz.Y0ksYNJ0FYvN91w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/en/gobankingrates_644/ec702bc1147bfe76112ce7f39f871388"),
-    p(" "),
-    p("-This page should provide a brief overview of your project: What major 
-      questions are you seeking to answer and what data will you use to answer 
-      those questions? You should include some \"additional flare\" on this 
-      landing page, such as an image."),
-    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-      eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    p(uiOutput('introduction')),
   )
 )
 
@@ -91,7 +81,7 @@ chart1_main_content <- mainPanel(plotlyOutput("chart1"))
 
 # Chart 1 page
 chart1_panel <- tabPanel(
-  title = "Chart 1",
+  title = "State Map",
   titlePanel("Choropleth Map of State Tax Information by County"),
   p("Write summary paragraph. Lorem ipsum dolor 
      sit amet, consectetur adipiscing elit, sed do 
@@ -135,11 +125,14 @@ chart2_main_content <- mainPanel(plotlyOutput("chart2"),
                                    in taxes. This is a fundamental rule of tax 
                                    brackets and is the reason why federal income
                                    tax rates increase as income increases. However,
-                                   this is not how taxes "))
+                                   this is not how taxes in the US seem to be distributed
+                                   in practice. The lower tax bracket should be charged 
+                                   the least percent of their income, but those earners actually have
+                                   almost the same tax burden as the top earners in some states. "))
 
 # Chart 2 page
 chart2_panel <- tabPanel(
-  title = "Chart 2",
+  title = "Taxes by Bracket",
   titlePanel("Assessment of Taxes by Bracket in US States"),
   sidebarLayout(
     chart2_sidebar_content,
@@ -213,8 +206,8 @@ chart3_main_content <- mainPanel(plotlyOutput("chart3"))
 
 # Chart 3 page
 chart3_panel <- tabPanel(
-  title = "Chart 3",
-  titlePanel("This is chart 3"),
+  title = "Scatter Plot",
+  titlePanel("Scatter Plot by Total Income"),
   p("Write summary paragraph. Lorem ipsum dolor 
      sit amet, consectetur adipiscing elit, sed do 
      eiusmod tempor incididunt ut labore et dolore 
@@ -235,34 +228,36 @@ chart3_panel <- tabPanel(
 ### SUMMARY PAGE
 summary_panel <- tabPanel(
   title = "Summary",
-  titlePanel("Summary Title"),
+  titlePanel("Our Findings"),
   fluidPage(
-    p("-A page that hones in on at least 3 major takeaways from the project 
-      (which should be related to a specific aspect of your analysis). Feel free 
-      to incorporate tables, graphics, or other elements to convey these conclusions."),
-    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-      eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    p("We find that high earners are using the capital gains 
+      tax laws to pay less tax on their income. This law is much more exploitable for high earners
+      who have the capital to fund portfolio investments. This is one of the most significant ways 
+      that wealthy individuals continue to become wealthier. This is an extremely relevant topic right now, 
+      as Joe Biden has proposed to raise the maximum capital gains tax from 20% to 39.6%. Therefore, for the highest bracket, 
+      capital gains will be effectively taxed as ordinary income. This will decrease the efficacy of 
+      this tax evasion strategy and promote equal paying of taxes."),
+    p("We also conclude that the tax system in the US does not provide enough benefits to the lower
+      class and furthers the cycle of poverty. The majority of the US is filing with income under $25,000, 
+      highlighting the high number of low earners in the US. Next, we look at total adjusted gross income. 
+      This is a metric that the IRS uses to calculate taxes by making slight adjustments to the total gross income. 
+      Interestingly, the AGI from tax bracket 2 is more than 3 and 4, suggesting that the middle class is shrinking. 
+      Unsurpsiringly, high earners in tax bracket 5 make up 37.09% of the total income in the US, while the lowest 
+      earners in bracket 1 (which make up almost 6x more returns) only account for 5.19% of income. 
+      Another statistic we can look at is tax payments. High earners (bracket 5) are paying upwards of 52.83% of total US taxes, 
+      almost 1 trillion US dollars. While the lowest earners only comprise 5.28% of total tax payments, they are still burdened 
+      with a 15.79% effective tax rate.")
   )
 )
 
 ### REPORT PAGE
 report_panel <- tabPanel(
   title = "Report",
-  titlePanel("Report Title"),
+  titlePanel("Report"),
   fluidPage(
-    p("-Iterate on your P01 and P02 to present your final report. Includes 
-      findings (new section), discussion  (new section), conclusion (new section), 
-      and, if needed, appendices. See P3 rubric."),
-    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-      eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    p(
+      uiOutput('report')
+)
   )
 )
 
